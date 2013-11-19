c283e2fb6b2e43d8ce7a445edf814145d1df5d02
yumaoywj <yuzhang.lille@gmail.com>
2013-11-20 03:20:38 +0800

    New UI from sofifa

--- platforms/ios/www/js/player-info.js
+++ platforms/ios/www/js/player-info.js
@@ -1,125 +1,85 @@
 $.support.cors = true;
 $.mobile.allowCrossDomainPages = true;
 
+/*function onDeviceReady() {
+	
+	if (parseFloat(window.device.version) === 7.0) {
+		document.body.style.marginTop = '20px';
+	}
+	
+	var db = window.openDatabase('yaoqiuyuan', '1.0', 'YaoQiuyuan', 1000000);
+}
+
+document.addEventListener('deviceready', onDeviceReady, false);*/
+
+var posClass = {
+	LW: 'F', ST: 'F', RW: 'F', LF: 'F', CF: 'F', RF: 'F',
+	CAM: 'M', LM: 'M', CM: 'M', RM: 'M', CDM: 'M',
+	LWB: 'D', LB: 'D', CB: 'D', RB: 'D', RWB: 'D', SW: 'D',
+	GK: 'G'
+};
+
+function getColorFromValue(value) {
+	if (value < 50) {
+		return 'red';
+	} else if (value < 65){
+		return 'orange';
+	} else if (value < 80) {
+		return 'yellow';
+	} else if (value < 90) {
+		return 'green';
+	} else {
+		return 'darkgreen';
+	}
+}
+
+function getColorFromPos(pos) {
+	switch (posClass[pos]) {
+	case 'F':
+		return 'blue';
+	case 'M':
+		return 'green';
+	case 'D':
+		return 'yellow';
+	case 'G':
+		return 'red';
+	default:
+		break;
+	}
+}
+
 $('#player-info').on('pageshow', function() {
 
-	function playerParser(aHTMLString) {
-		var parser = new DOMParser();
-		var doc = parser.parseFromString(aHTMLString, "text/html");
-		var $article = $(doc).find('article');
-		
-		var player = {};
-		player['avatar'] = $article.find('h1 img').attr('data-frz-src');
-		player['sname'] = $.trim($article.find('h1').text());
-		player['nation'] = $article.find('.n-flag').attr('class').split(' ')[1].substr(2);
-
-		var $lis = $article.find('ul').eq(0).find('li');
-		player['fname'] = $.trim($lis.eq(0).text());
-		player['birthday'] = $lis.eq(2).text().match(/\([^\)]+\)/)[0].slice(1, -1);
-		player['height'] = $lis.eq(3).text().substr(0, 4) - 0;
-		player['weight'] = $lis.eq(3).text().split(' ')[1].substr(0, 2) - 0;
-		player['overall'] = $lis.eq(4).find('.prop').text() - 0;
-		player['potential'] = $lis.eq(5).find('.prop').text() - 0;
-		player['position'] = [];
-		$lis.eq(6).find('.pos').each(function() {
-			player['position'].push($(this).text());
-		});
-		player['foot'] = $lis.eq(7).text().split('\n')[2];
-		player['reputation'] = $lis.eq(8).find('img').attr('title') - 0;
-		player['weakfoot'] = $lis.eq(9).find('img').attr('title') - 0;
-		player['skillmoves'] = $lis.eq(10).find('img').attr('title') - 0;
-		player['awr'] = $lis.eq(11).find('span').text();
-		player['dwr'] = $lis.eq(12).find('span').text();
-		player['id'] = $lis.eq(13).text().split('\n')[2] - 0;
-
-		var $club = $article.find('.player-team').eq(0);
-		player['club'] = {
-			name: $club.find('h6').text(),
-			logo: $club.find('.logo').attr('src')
-		};
-
-		var $attr = $article.find('.total_percent').parent().parent();
-		$lis = $attr.eq(0).find('li');
-		player['attacking'] = {
-			crossing: $lis.eq(0).find('.prop').text() - 0,
-			finishing: $lis.eq(1).find('.prop').text() - 0,
-			heading: $lis.eq(2).find('.prop').text() - 0,
-			spassing: $lis.eq(3).find('.prop').text() - 0,
-			volleys: $lis.eq(4).find('.prop').text() - 0
-		};
-		$lis = $attr.eq(1).find('li');
-		player['power'] = {
-			shotpower: $lis.eq(0).find('.prop').text() - 0,
-			jumping: $lis.eq(1).find('.prop').text() - 0,
-			stamina: $lis.eq(2).find('.prop').text() - 0,
-			strength: $lis.eq(3).find('.prop').text() - 0,
-			longshots: $lis.eq(4).find('.prop').text() - 0
-		};
-		$lis = $attr.eq(2).find('li');
-		player['defending'] = {
-			marking: $lis.eq(0).find('.prop').text() - 0,
-			standing: $lis.eq(1).find('.prop').text() - 0,
-			sliding: $lis.eq(2).find('.prop').text() - 0
-		};
-		$lis = $attr.eq(3).find('li');
-		player['mentality'] = {
-			aggressing: $lis.eq(0).find('.prop').text() - 0,
-			interceptions: $lis.eq(1).find('.prop').text() - 0,
-			positioning: $lis.eq(2).find('.prop').text() - 0,
-			vision: $lis.eq(3).find('.prop').text() - 0,
-			penalties: $lis.eq(4).find('.prop').text() - 0
-		};
-		$lis = $attr.eq(4).find('li');
-		player['skill'] = {
-			dribbling: $lis.eq(0).find('.prop').text() - 0,
-			curve: $lis.eq(1).find('.prop').text() - 0,
-			freekick: $lis.eq(2).find('.prop').text() - 0,
-			lpassing: $lis.eq(3).find('.prop').text() - 0,
-			control: $lis.eq(4).find('.prop').text() - 0
-		};
-		$lis = $attr.eq(5).find('li');
-		player['movement'] = {
-			acceleration: $lis.eq(0).find('.prop').text() - 0,
-			speed: $lis.eq(1).find('.prop').text() - 0,
-			agility: $lis.eq(2).find('.prop').text() - 0,
-			reactions: $lis.eq(3).find('.prop').text() - 0,
-			balance: $lis.eq(4).find('.prop').text() - 0
-		};
-		var $uls = $attr.eq(6).find('ul');
-		$lis = $uls.eq(0).find('li');
-		player['gk'] = {
-			diving: $lis.eq(0).find('.prop').text() - 0,
-			handling: $lis.eq(1).find('.prop').text() - 0,
-			kicking: $lis.eq(2).find('.prop').text() - 0,
-			gkposition: $lis.eq(3).find('.prop').text() - 0,
-			reflexes: $lis.eq(4).find('.prop').text() - 0
-		};
-
-		$lis = $uls.eq(1).find('li');
-		player['traits'] = [];
-		$lis.each(function() {
-			player['traits'].push($(this).text());
-		});
-
-		$lis = $uls.eq(2).find('li');
-		player['specialities'] = [];
-		$lis.each(function() {
-			player['specialities'].push($.trim($(this).text()));
-		});
-
-		return player;
-	}
-
 	function fillInPage(player) {
+		$('.player-sname').text(player.sname);
+		$('.c-logo').attr('src', player.club.logo);
+		$('.c-name').text(' ' +　player.club.name);
+
+		$('.player-avatar').attr('src', player.avatar);
+		$('.n-flag').attr('class', 'n-flag n-' + player.nation.id);
+		$('.n-name').text(' ' +　player.nation.name);
+		$('.player-birthday').text(player.birthday);
+		$('.player-height').text(player.height + 'cm');
+		$('.player-weight').text(player.weight + 'kg');
+
+		$('.player-overall').text(player.overall).attr('class', 'player-overall value '+ getColorFromValue(player.overall) +'-bg');
+		$('.player-potential').text(player.potential).attr('class', 'player-potential value '+ getColorFromValue(player.potential) +'-bg');
+
+		var $playerPosition = $('.player-position');
+		for (var i = 0, len = player.position.length; i < len; ++i) {
+			(function() {
+				var pos = player.position[i];
+				console.log(pos);
+				$playerPosition.append('<span class="badge ' + getColorFromPos(pos) + '-bg">' + pos + '</span> ');
+			})();
+		}
+
 		
 	}
 
-	$.get('http://sofifa.com/cn/14w/p/148290-mesut-ozil', function(data) {
-		console.log('got it !');
-		var player = playerParser(data);
-		console.log(player);
-		fillInPage(player);
-	});
+	var player = JSON.parse('{"avatar":"http://cdn.content.easports.com/fifa/fltOnlineAssets/C74DDF38-0B11-49b0-B199-2E2A11D1CC13/2014/fut/items/images/players/web/20801.png","sname":"Cristiano Ronaldo","fname":"C. Ronaldo dos Santos Aveiro","nation":{"id":"38","name":"葡萄牙"},"birthday":"05 February 1985","height":185,"weight":80,"overall":92,"potential":95,"position":["LW","LM"],"foot":"右","id":20801,"reputation":5,"weakfoot":4,"skillmoves":4,"awr":"高","dwr":"低","club":{"name":"皇家马德里","logo":"http://cache.images.globalsportsmedia.com/soccer/teams/150x150/2016.png"},"attacking":{"crossing":83,"finishing":92,"heading":86,"spassing":82,"volleys":85},"skill":{"dribbling":90,"curve":88,"freekick":79,"lpassing":72,"control":95},"movement":{"acceleration":91,"speed":94,"agility":93,"reactions":90,"balance":75},"power":{"shotpower":94,"jumping":94,"stamina":89,"strength":79,"longshots":93},"mentality":{"aggressing":63,"interceptions":24,"positioning":89,"vision":81,"penalties":85},"defending":{"marking":22,"standing":31,"sliding":23},"gk":{"diving":7,"handling":11,"kicking":15,"gkposition":14,"reflexes":11},"traits":["大力任意球","被侵犯易倒地(假摔)","天才","百步穿杨","善于盘带"],"specialities":["Speedster","Dribbler","Distance Shooter","Acrobat","Clinical Finisher","Complete Forward"]}');
+	console.log(player);
+	fillInPage(player);
 });
 
 	
